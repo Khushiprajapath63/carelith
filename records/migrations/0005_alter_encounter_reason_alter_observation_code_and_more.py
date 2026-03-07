9efc856b496a4e4a58ae509cfbd4e62eb6ccae86
khushi <khushi.gmail>
2026-03-07 17:19:52 +0530

    fixed migrations

--- records/migrations/0005_alter_encounter_reason_alter_observation_code_and_more.py
+++
@@ -1,9 +0,0 @@
-from django.db import migrations
-
-class Migration(migrations.Migration):
-
-    dependencies = [
-        ("records", "0004_add_created_at_to_report"),
-    ]
-
-    operations = []
--- records/migrations/0005_dummy_fix.py
+++
@@ -1,9 +0,0 @@
-from django.db import migrations
-
-class Migration(migrations.Migration):
-
-    dependencies = [
-        ("records", "0004_remove_report_doctor_explanation_and_more"),
-    ]
-
-    operations = []
