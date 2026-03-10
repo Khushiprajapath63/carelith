4ad4a86d203587ada1caae3254c212144f54d2ef
khushi <khushi.gmail>
2026-03-10 21:02:42 +0530

    fix email crash

--- doctor_app/views.py
+++ doctor_app/views.py
@@ -171,14 +171,19 @@ def request_patient_access(request, patient_id):
         is_verified=False,
         expires_at=expiry_time
     )
-    print("OTP:", otp)
-    send_mail(
-        subject="Carelith OTP Verification",
-        message=f"Hello {patient.user.username},\n\nYour OTP is: {otp}\n\nValid for 10 minutes.",
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=[patient.user.email],
-        fail_silently=True,
-    )
+
+    print("OTP GENERATED:", otp)
+
+    try:
+        send_mail(
+            subject="Carelith OTP Verification",
+            message=f"Hello {patient.user.username},\n\nYour OTP is: {otp}\n\nValid for 10 minutes.",
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[patient.user.email],
+            fail_silently=False,
+        )
+    except Exception as e:
+        print("EMAIL ERROR:", e)
 
     return redirect("doctor_app:verify_patient_otp", access_id=access_obj.id)
 
