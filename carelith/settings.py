ced4171ccc6e91334851800bc310e4fc5aaa2573
khushi <khushi.gmail>
2026-03-11 19:11:48 +0530

    Fix media file storage

--- carelith/settings.py
+++ carelith/settings.py
@@ -115,8 +115,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 # Default primary key field type
