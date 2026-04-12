5fd932af58533a366795e8684b8f2154c61a1b0c
khushi <khushi.gmail>
2026-04-12 13:48:54 +0530

    fix DEBUG setting to read from environment

--- carelith/settings.py
+++ carelith/settings.py
@@ -5,7 +5,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ["carelith-9dab.onrender.com"]
 INSTALLED_APPS = [
