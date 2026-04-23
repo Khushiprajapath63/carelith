ddd9e8cb787ec38aae6b2f840744235bcaecd673
khushi <khushi.gmail>
2026-04-23 17:27:24 +0530

    feat: add prescription template and lab user field

--- laboratory/models.py
+++ laboratory/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Laboratory(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=15, null=True, blank=True)
     address = models.TextField(null=True, blank=True)
