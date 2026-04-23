413159f522b6bdb4d88c0a8da44eaafab8fbf708
khushi <khushi.gmail>
2026-04-23 17:15:43 +0530

    feat: fix lab model, add user field and lab redirect

--- doctor_app/urls.py
+++ doctor_app/urls.py
@@ -6,13 +6,10 @@ app_name = "doctor_app"
 urlpatterns = [
     path("dashboard/", views.doctor_dashboard, name="doctor_dashboard"),
     path("report/<int:report_id>/explain/", views.explain_report, name="explain_report"),
-
     path("patient/<int:patient_id>/request-access/", views.request_patient_access, name="request_patient_access"),
     path("verify-otp/<int:access_id>/", views.verify_patient_otp, name="verify_patient_otp"),
-
-    # ✅ FHIR URLs
     path("patient/<int:patient_id>/fhir-records/", views.view_patient_fhir_records, name="view_patient_fhir_records"),
     path("patient/<int:patient_id>/upload-fhir-report/", views.upload_patient_report_to_fhir, name="upload_patient_report_to_fhir"),
-    path("settings/",views.doctor_settings,name="doctor_settings")
-    path('patient/<int:patient_id>/write-prescription/', views.write_prescription, name='write_prescription'),
+    path("settings/", views.doctor_settings, name="doctor_settings"),
+    path("patient/<int:patient_id>/write-prescription/", views.write_prescription, name="write_prescription"),
 ]
